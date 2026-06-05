69b996f6490c072102aac480c6fafea3a7cfb0da
Viktor <viktor@example.com>
2026-06-05 17:22:49 +0300

    Add mailings templates (list, detail, form, delete) with Bootstrap styling

--- email_messages/views.py
+++ email_messages/views.py
@@ -1,11 +1,14 @@
 from django.urls import reverse_lazy
-from django.views.generic import ListView, CreateView, UpdateView, DeleteView
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView, DetailView
+
 from .models import Message
+
 
 class MessageListView(ListView):
     model = Message
     template_name = 'email_messages/message_list.html'
     context_object_name = 'messages'
+
 
 class MessageCreateView(CreateView):
     model = Message
@@ -13,13 +16,21 @@ class MessageCreateView(CreateView):
     template_name = 'email_messages/message_form.html'
     success_url = reverse_lazy('email_messages:list')
 
+
 class MessageUpdateView(UpdateView):
     model = Message
     fields = ['subject', 'body']
     template_name = 'email_messages/message_form.html'
     success_url = reverse_lazy('email_messages:list')
 
+
 class MessageDeleteView(DeleteView):
     model = Message
     template_name = 'email_messages/message_confirm_delete.html'
     success_url = reverse_lazy('email_messages:list')
+
+
+class MessageDetailView(DetailView):
+    model = Message
+    template_name = 'email_messages/message_detail.html'
+    context_object_name = 'message'
