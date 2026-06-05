577aed83716f1a194ba8bd2332653dcd52ada9dc
Viktor <viktor@example.com>
2026-06-05 14:06:57 +0300

    Add clients app with Client model and CRUD views

---
+++ clients/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+class Client(models.Model):
+    email = models.EmailField(unique=True, verbose_name="Email")
+    full_name = models.CharField(max_length=255, verbose_name="Ф. И. О.")
+    comment = models.TextField(blank=True, verbose_name="Комментарий")
+
+    def __str__(self):
+        return self.full_name or self.email
