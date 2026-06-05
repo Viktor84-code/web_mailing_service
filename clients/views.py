915e56ace68b88ba2c6a876c2cc5652d0d097283
Viktor <viktor@example.com>
2026-06-05 15:01:56 +0300

    Add main app with home page, update settings and urls

--- clients/views.py
+++ clients/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
+from .models import Client
 
-# Create your views here.
+class ClientListView(ListView):
+    model = Client
+    template_name = 'clients/client_list.html'
+    context_object_name = 'clients'
+
+class ClientCreateView(CreateView):
+    model = Client
+    fields = ['email', 'full_name', 'comment']
+    template_name = 'clients/client_form.html'
+    success_url = reverse_lazy('clients:list')
+
+class ClientUpdateView(UpdateView):
+    model = Client
+    fields = ['email', 'full_name', 'comment']
+    template_name = 'clients/client_form.html'
+    success_url = reverse_lazy('clients:list')
+
+class ClientDeleteView(DeleteView):
+    model = Client
+    template_name = 'clients/client_confirm_delete.html'
+    success_url = reverse_lazy('clients:list')
